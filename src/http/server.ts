dc2cf0b4e62c173437a6b58e7ecc3f1687ca5a47
Jean F <jeanfabiogruber2109@gmail.com>
2024-11-02 20:31:16 -0300

    feat: change server port to 3333 for local development

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,7 +3,7 @@ import { AppDataSource } from '../data-source';
 import routes from "../http/routes/routes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3333;
 
 AppDataSource.initialize()
     .then(() => {
